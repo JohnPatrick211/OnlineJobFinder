eb333900e00d14833b05a43e08e63da023158d5a
wynspogi211 <alvezwynsjohn@gmail.com>
2021-08-15 12:35:19 +0800

    email verification link to OTP email verification in applicant registration module is complete

--- app/src/main/java/com/example/onlinejobfinder/tabregister/ApplicantFragment.java
+++ app/src/main/java/com/example/onlinejobfinder/tabregister/ApplicantFragment.java
@@ -26,8 +26,11 @@ import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.onlinejobfinder.Constant;
+import com.example.onlinejobfinder.EmailActivity;
 import com.example.onlinejobfinder.MainActivity;
 import com.example.onlinejobfinder.R;
+import com.example.onlinejobfinder.SendMail;
+import com.example.onlinejobfinder.SendOTPMail;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
@@ -42,6 +45,7 @@ import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 
 /**
@@ -55,8 +59,11 @@ public class ApplicantFragment extends Fragment {
      TextInputLayout layoutEmail,layoutPassword,layoutConfirm,layoutName;
     TextInputEditText edt_email,edt_name,edt_password,edt_confirmpassword;
     ProgressDialog progressDialog;
-    FirebaseAuth mAuth;
-    FirebaseUser user;
+//    FirebaseAuth mAuth;
+//    FirebaseUser user;
+    int randomnumber;
+    String intentOTPcode;
+
 
 
     // TODO: Rename parameter arguments, choose names that match
@@ -115,10 +122,11 @@ public class ApplicantFragment extends Fragment {
         layoutConfirm = view.findViewById(R.id.txtLayoutConfirmPasswordSignUp);
         progressDialog = new ProgressDialog(getContext());
         progressDialog.setCancelable(false);
-        mAuth = FirebaseAuth.getInstance();
+//        mAuth = FirebaseAuth.getInstance();
         String role_applicant = "applicant";
-        user = mAuth.getCurrentUser();
+//        user = mAuth.getCurrentUser();
         signin = view.findViewById(R.id.txtview_signin);
+
         signin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -210,47 +218,29 @@ public class ApplicantFragment extends Fragment {
             public void onClick(View view) {
                 if(validate())
                 {
-                    //                Toast.makeText(getContext(),"login success",Toast.LENGTH_SHORT).show();
-                    progressDialog.setMessage("Registering");
+                    Random random = new Random();
+                    randomnumber = random.nextInt(999999-111111) + 111111;
+                    progressDialog.setMessage("Checking Email");
                     progressDialog.show();
-                    StringRequest request = new StringRequest(Request.Method.POST, Constant.register, response -> {
+                    StringRequest request = new StringRequest(Request.Method.POST, Constant.checkemail, response -> {
                         try{
                             JSONObject object= new JSONObject(response);
-                            if(object.getBoolean("success")){
-                                mAuth.createUserWithEmailAndPassword(edt_email.getText().toString().trim(),edt_password.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                                    @Override
-                                    public void onComplete(@NonNull Task<AuthResult> task) {
-                                        if(task.isSuccessful())
-                                        {
-                                            sendVerificationEmail();
-                                        }
-                                    }
-                                }).addOnFailureListener(new OnFailureListener() {
-                                    @Override
-                                    public void onFailure(@NonNull Exception e) {
-                                        Toast.makeText(getContext(),e.getMessage(),Toast.LENGTH_SHORT).show();
-                                        Log.e("MYAPP", "exception", e);
-                                    }
-                                });
-                                JSONObject user = object.getJSONObject("user");
-                                SharedPreferences userPref = getActivity().getApplicationContext().getSharedPreferences("user",getContext().MODE_PRIVATE);
-                                SharedPreferences.Editor editor = userPref.edit();
-                                editor.putString("token",object.getString("token"));
-                                editor.putString("email",user.getString("email"));
-                                editor.putString("name",user.getString("name"));
-                                editor.putString("role",user.getString("role"));
-                                editor.apply();
-//                            Toast.makeText(getContext(),"Register Successfully",Toast.LENGTH_SHORT).show();
-                                progressDialog.cancel();
-                            }
-                            else if(object.getString("Status").equals("201"))
+                            if(object.getString("Status").equals("201"))
                             {
-                                Toast.makeText(getContext(),"Email Already Exists, Please Try Again",Toast.LENGTH_SHORT).show();
+                                SendOTPMail sm = new SendOTPMail(getContext(), edt_email.getText().toString().trim(), "Email Verification", "Your Code is " +randomnumber,edt_email.getText().toString().trim());
+                                sm.execute();
+                                Intent i = new Intent(getContext(), EmailOTPActivity.class);
+                                i.putExtra("name",edt_name.getText().toString().trim());
+                                i.putExtra("email",edt_email.getText().toString().trim());
+                                i.putExtra("password",edt_password.getText().toString());
+                                i.putExtra("OTPcode", Integer.toString(randomnumber));
+                                i.putExtra("role",role_applicant);
+                                startActivity(i);
                                 progressDialog.cancel();
                             }
                             else if(object.getString("Status").equals("202"))
                             {
-                                Toast.makeText(getContext(),"Name Already Exists, Please Try Again",Toast.LENGTH_SHORT).show();
+                                Toast.makeText(getContext(),"Email Already Used, Please Try Again",Toast.LENGTH_SHORT).show();
                                 progressDialog.cancel();
                             }
                             else
@@ -261,7 +251,7 @@ public class ApplicantFragment extends Fragment {
 
                         }catch(JSONException e)
                         {
-                            Toast.makeText(getContext(),"error",Toast.LENGTH_SHORT).show();
+                            Toast.makeText(getContext(),response,Toast.LENGTH_SHORT).show();
                             progressDialog.cancel();
                         }
                     },error ->{
@@ -271,45 +261,46 @@ public class ApplicantFragment extends Fragment {
                         @Override
                         protected Map<String, String> getParams() throws AuthFailureError {
                             HashMap<String,String> map = new HashMap<>();
-                            map.put("name",edt_name.getText().toString().trim());
+//                        map.put("name",edt_nameemp.getText().toString().trim());
                             map.put("email",edt_email.getText().toString().trim());
-                            map.put("password",edt_password.getText().toString());
-                            map.put("role",role_applicant);
+//                        map.put("password",edt_passwordemp.getText().toString());
+//                        map.put("role",role_employer);
                             return map;
                         }
                     };
                     RequestQueue queue = Volley.newRequestQueue(getContext());
                     queue.add(request);
+                    //                Toast.makeText(getContext(),"login success",Toast.LENGTH_SHORT).show();
                 }
             }
         });
         return view;
     }
 
-    private void sendVerificationEmail() {
-        if(mAuth.getCurrentUser()!=null)
-        {
-            mAuth.getCurrentUser().sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
-                @Override
-                public void onComplete(@NonNull Task<Void> task) {
-                    if(task.isSuccessful())
-                    {
-                        Toast.makeText(getContext(),"Email has been send to your email address to verify, if you verified, proceed to login by clicking login below",Toast.LENGTH_SHORT).show();
-//                        user.reload();
-                    }
-                    else
-                    {
-                        Toast.makeText(getContext(),"failed to send verification email",Toast.LENGTH_SHORT).show();
-                    }
-                }
-            }).addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
-                    Toast.makeText(getContext(),e.getMessage(),Toast.LENGTH_SHORT).show();
-                }
-            });
-        }
-    }
+//    private void sendVerificationEmail() {
+//        if(mAuth.getCurrentUser()!=null)
+//        {
+//            mAuth.getCurrentUser().sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+//                @Override
+//                public void onComplete(@NonNull Task<Void> task) {
+//                    if(task.isSuccessful())
+//                    {
+//                        Toast.makeText(getContext(),"Email has been send to your email address to verify, if you verified, proceed to login by clicking login below",Toast.LENGTH_SHORT).show();
+////                        user.reload();
+//                    }
+//                    else
+//                    {
+//                        Toast.makeText(getContext(),"failed to send verification email",Toast.LENGTH_SHORT).show();
+//                    }
+//                }
+//            }).addOnFailureListener(new OnFailureListener() {
+//                @Override
+//                public void onFailure(@NonNull Exception e) {
+//                    Toast.makeText(getContext(),e.getMessage(),Toast.LENGTH_SHORT).show();
+//                }
+//            });
+//        }
+//    }
     private boolean validate (){
         if (edt_name.getText().toString().isEmpty()){
             layoutName.setErrorEnabled(true);
---
+++ app/src/main/java/com/example/onlinejobfinder/tabregister/EmailOTPActivity.java
@@ -0,0 +1,308 @@
+package com.example.onlinejobfinder.tabregister;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.onlinejobfinder.Constant;
+import com.example.onlinejobfinder.EmailActivity;
+import com.example.onlinejobfinder.MainActivity;
+import com.example.onlinejobfinder.R;
+import com.example.onlinejobfinder.SendOTPMail;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+public class EmailOTPActivity extends AppCompatActivity {
+
+    EditText code1, code2, code3, code4, code5, code6;
+    Button btnEmailVerify;
+    TextView displayemail,tv_resend;
+    ProgressDialog progressDialog;
+    int i = 30;
+    CountDownTimer CDT;
+    int randomnumber;
+    String intentOTPcode;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_email_o_t_p);
+        displayemail = findViewById(R.id.textemail);
+        code1 = findViewById(R.id.inputCode1);
+        code2 = findViewById(R.id.inputCode2);
+        code3 = findViewById(R.id.inputCode3);
+        code4 = findViewById(R.id.inputCode4);
+        code5 = findViewById(R.id.inputCode5);
+        code6 = findViewById(R.id.inputCode6);
+        tv_resend = findViewById(R.id.textResendOTP);
+        btnEmailVerify = findViewById(R.id.btnVerifyEmailOTP);
+        progressDialog = new ProgressDialog(EmailOTPActivity.this);
+        progressDialog.setCancelable(false);
+
+        String intentname = getIntent().getExtras().getString("name");
+        String intentemail = getIntent().getExtras().getString("email");
+        String intentpassword = getIntent().getExtras().getString("password");
+        String intentrole = getIntent().getExtras().getString("role");
+        intentOTPcode = getIntent().getExtras().getString("OTPcode");
+//        Toast.makeText(EmailOTPActivity.this,intentOTPcode,Toast.LENGTH_SHORT).show();
+        displayemail.setText(intentemail);
+
+        Random random = new Random();
+        randomnumber = random.nextInt(999999-111111) + 111111;
+
+        setupOTPInputs();
+        tv_resend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SendOTPMail sm = new SendOTPMail(EmailOTPActivity.this, intentemail, "Email Verification", "Your Code is " +randomnumber ,intentemail);
+                intentOTPcode = Integer.toString(randomnumber);
+                sm.execute();
+                CDT = new CountDownTimer(30000, 1000) {
+                    @Override
+                    public void onTick(long l) {
+                        tv_resend.setText("Resend In " + i + " sec");
+                        tv_resend.setEnabled(false);
+                        i--;
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        tv_resend.setText("Resend Here");
+                        tv_resend.setEnabled(true);
+                        i=30;
+                    }
+                }.start();
+            }
+        });
+
+        btnEmailVerify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(validate())
+                {
+                    String code = code1.getText().toString() +
+                            code2.getText().toString() +
+                            code3.getText().toString() +
+                            code4.getText().toString() +
+                            code5.getText().toString() +
+                            code6.getText().toString();
+                    if(code.equals(intentOTPcode))
+                    {
+                        if(intentrole.equals("applicant"))
+                        {
+                            progressDialog.setMessage("Registering");
+                            progressDialog.show();
+                            StringRequest request = new StringRequest(Request.Method.POST, Constant.register, response -> {
+                                try{
+                                    JSONObject object= new JSONObject(response);
+                                    if(object.getBoolean("success")){
+//                                mAuth.createUserWithEmailAndPassword(edt_email.getText().toString().trim(),edt_password.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+//                                    @Override
+//                                    public void onComplete(@NonNull Task<AuthResult> task) {
+//                                        if(task.isSuccessful())
+//                                        {
+//                                            sendVerificationEmail();
+//                                        }
+//                                    }
+//                                }).addOnFailureListener(new OnFailureListener() {
+//                                    @Override
+//                                    public void onFailure(@NonNull Exception e) {
+//                                        Toast.makeText(getContext(),e.getMessage(),Toast.LENGTH_SHORT).show();
+//                                        Log.e("MYAPP", "exception", e);
+//                                    }
+//                                });
+                                        Intent i = new Intent(EmailOTPActivity.this, MainActivity.class);
+                                        startActivity(i);
+                                        Toast.makeText(EmailOTPActivity.this,"Register Successfully",Toast.LENGTH_SHORT).show();
+                                        JSONObject user = object.getJSONObject("user");
+                                        SharedPreferences userPref = getApplicationContext().getSharedPreferences("user",MODE_PRIVATE);
+                                        SharedPreferences.Editor editor = userPref.edit();
+                                        editor.putString("token",object.getString("token"));
+                                        editor.putString("email",user.getString("email"));
+                                        editor.putString("name",user.getString("name"));
+                                        editor.putString("role",user.getString("role"));
+                                        editor.apply();
+//                            Toast.makeText(getContext(),"Register Successfully",Toast.LENGTH_SHORT).show();
+                                        progressDialog.cancel();
+                                    }
+                                    else if(object.getString("Status").equals("201"))
+                                    {
+                                        Toast.makeText(EmailOTPActivity.this,"Email Already Exists, Please Try Again",Toast.LENGTH_SHORT).show();
+                                        progressDialog.cancel();
+                                    }
+                                    else if(object.getString("Status").equals("202"))
+                                    {
+                                        Toast.makeText(EmailOTPActivity.this,"Name Already Exists, Please Try Again",Toast.LENGTH_SHORT).show();
+                                        progressDialog.cancel();
+                                    }
+                                    else
+                                    {
+                                        Toast.makeText(EmailOTPActivity.this, "Error Occurred, Please try again", Toast.LENGTH_SHORT).show();
+                                        progressDialog.cancel();
+                                    }
+
+                                }catch(JSONException e)
+                                {
+                                    Toast.makeText(EmailOTPActivity.this,"error",Toast.LENGTH_SHORT).show();
+                                    progressDialog.cancel();
+                                }
+                            },error ->{
+                                error.printStackTrace();
+                            })
+                            {
+                                @Override
+                                protected Map<String, String> getParams() throws AuthFailureError {
+                                    HashMap<String,String> map = new HashMap<>();
+                                    map.put("name",intentname);
+                                    map.put("email",intentemail);
+                                    map.put("password",intentpassword);
+                                    map.put("role",intentrole);
+                                    return map;
+                                }
+                            };
+                            RequestQueue queue = Volley.newRequestQueue(EmailOTPActivity.this);
+                            queue.add(request);
+                        }
+                        else
+                        {
+                            Toast.makeText(EmailOTPActivity.this,"Employer verification",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    else
+                    {
+                        Toast.makeText(EmailOTPActivity.this,"Wrong Code, Please Try Again",Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+    }
+    private void setupOTPInputs()
+    {
+        code1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(!charSequence.toString().trim().isEmpty())
+                {
+                    code2.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        code2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(!charSequence.toString().trim().isEmpty())
+                {
+                    code3.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        code3.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(!charSequence.toString().trim().isEmpty())
+                {
+                    code4.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        code4.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(!charSequence.toString().trim().isEmpty())
+                {
+                    code5.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        code5.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(!charSequence.toString().trim().isEmpty())
+                {
+                    code6.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+    }
+    private boolean validate()
+    {
+        if(code1.getText().toString().trim().isEmpty() || code2.getText().toString().trim().isEmpty() || code3.getText().toString().trim().isEmpty() || code4.getText().toString().trim().isEmpty() || code5.getText().toString().trim().isEmpty() || code6.getText().toString().trim().isEmpty())
+        {
+            Toast.makeText(EmailOTPActivity.this,"Please Enter Valid Code",Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+}
