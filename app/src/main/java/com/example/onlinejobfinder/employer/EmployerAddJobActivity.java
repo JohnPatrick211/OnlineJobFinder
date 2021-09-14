df47a97092d3c8bd65d8af07d8d0f18aa0394df9
wynspogi211 <alvezwynsjohn@gmail.com>
2021-09-14 09:02:49 +0800

    Bug fixed in design of logo and drawer navigation

--- app/src/main/java/com/example/onlinejobfinder/employer/EmployerAddJobActivity.java
+++ app/src/main/java/com/example/onlinejobfinder/employer/EmployerAddJobActivity.java
@@ -16,6 +16,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -42,7 +43,7 @@ import java.util.Map;
 
 public class EmployerAddJobActivity extends AppCompatActivity {
 
-    CircleImageView joblogo;
+    ImageView joblogo;
     EditText jobtitle,jobaddress,jobsalary,jobdescription,jobcompanyoverview;
     TextView jobcompanyname, jobregion,jobspecialization,jobemail;
     Button btnsendjobapprove,btndelete;
